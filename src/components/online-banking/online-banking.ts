70daa2d65453b6e991e85f1565640c096a055107
Gunko Vika <vikahunko@icloud.com>
2026-08-17 02:50:38 +0300

    feat: add online banking section

---
+++ src/components/online-banking/online-banking.ts
@@ -0,0 +1,10 @@
+import './online-banking.css'
+
+export function createOnlineBanking(): HTMLElement {
+    const section = document.createElement('section')
+    section.className = 'online-banking'
+
+
+
+    return section
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './style.css'
 import { createHeader } from './components/header/header'
 import { createHero } from './components/hero/hero'
+import { createOnlineBanking } from './components/online-banking/online-banking'
+
 
 const app = document.querySelector<HTMLDivElement>('#app')
 if (!app) {
@@ -13,11 +15,10 @@ page.className = 'page'
 const header = createHeader()
 const introScreen = createHero(header)
 
-const contentSection = document.createElement('section')
-contentSection.className = 'content-section'
+const onlineBanking = createOnlineBanking()
 
 const footer = document.createElement('footer')
 footer.className = 'footer'
 
-page.append(introScreen, contentSection, footer)
+page.append(introScreen, onlineBanking, footer)
 app.append(page)
